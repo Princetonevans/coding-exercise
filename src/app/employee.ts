62d6a64521b713dfb1174d5bc4786396b1f257bd
Princeton_Evans <princetonevans@yahoo.com>
2023-06-17 20:35:24 -0500

    created employee interface

---
+++ src/app/employee.ts
@@ -0,0 +1,11 @@
+export interface Employee {
+  department: string;
+  avatar: string;
+  dateOfBirth: string;
+  dateOfHire: string;
+  firstName: string;
+  id: number;
+  lastName: string;
+  married: boolean;
+  position: string;
+}
