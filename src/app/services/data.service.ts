eec2c4126e71e0668e437f2d7bd45913888229b5
Princeton_Evans <princetonevans@yahoo.com>
2023-06-17 20:36:15 -0500

    created data service to retrieve data

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
+import { Observable, catchError, tap, throwError, map } from 'rxjs';
+import { Employee } from '../employee';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  private employeeUrl = 'api/v2/employees';
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': 'true',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
+    }),
+  };
+  constructor(private http: HttpClient) {}
+
+  //Method called to get list an array of Employees
+  getAllEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(this.employeeUrl, this.httpOptions).pipe(
+      map((employees) => {
+        employees.sort((a, b) => (a.dateOfHire < b.dateOfHire ? -1 : 1));
+        return employees;
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(err: HttpErrorResponse): Observable<never> {
+    // in a real world app, we may send the server to some remote logging infrastructure
+    // instead of just logging it to the console
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(() => errorMessage);
+  }
+}
